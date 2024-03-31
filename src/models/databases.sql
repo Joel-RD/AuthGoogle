5f25cc4ee34d9f340762dfef7412f05dddc85b10
JoelRD:01 <eudy.joel.reyes@gmail.com>
2024-03-31 15:37:08 -0400

    Images integration

--- src/models/databases.sql
+++ src/models/databases.sql
@@ -4,8 +4,8 @@ CREATE DATABASE AppClient;
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
-    price INTEGER  NOT NULL,
-    filename TEXT UNIQUE
+    email TEXT UNIQUE NOT NULL,
+    filename TEXT UNIQUE NOT NULL,
 );
 
 INSERT INTO users (name, email, filename) VALUES ('John Doe', 'john@example.com', 'profile.jpg');
