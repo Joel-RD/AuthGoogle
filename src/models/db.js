1be722bd43fb56ad6080af40f248e7acc689c2a5
JoelRD:01 <eudy.joel.reyes@gmail.com>
2024-03-31 15:39:30 -0400

    Images integration

--- src/models/db.js
+++ src/models/db.js
@@ -10,7 +10,7 @@ const { VERCEL_CONNECTION, LOCAL_CONNECTION } = process.env;
 export const db = () => {
   try {
     return new Pool({
-      connectionString:  LOCAL_CONNECTION,
+      connectionString:  VERCEL_CONNECTION || LOCAL_CONNECTION,
     });
   } catch (error) {
     console.log(error);
